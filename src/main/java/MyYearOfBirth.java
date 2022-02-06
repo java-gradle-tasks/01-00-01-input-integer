a389d2a4d98e2539ddf0817dd2647bcc2eed9fac
Gyuris Csaba <gycsaba72@gmail.com>
2022-02-06 16:15:46 +0100

    Feladat megoldásának törlése

--- src/main/java/MyYearOfBirth.java
+++ src/main/java/MyYearOfBirth.java
@@ -5,11 +5,6 @@ public class MyYearOfBirth {
 
 	public static void main(final String[] args) {
 		// Write your solution here
-		System.out.print("Adja meg a szuletesi evet:");
-		Scanner in=new Scanner(System.in);
-		int yearOfBirth=in.nextInt();
-		System.out.println(yearOfBirth);
-		in.close();
 	}
 
 }
